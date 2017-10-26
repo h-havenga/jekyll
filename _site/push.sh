af69766355f66605c1f52d18025e4a208580780b
Henno Havenga <hhavenga92@gmail.com>
2017-10-26 12:31:44 +0200

    bash

--- _site/push.sh
+++ _site/push.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+echo "Press crtl-c when prompt "
+
 bundle exec jekyll serve
 git add .
-git commit -m "first commit"
+
+echo -n "What do you want to name your commit? [ENTER]: "
+read name
+
+git commit -m "$name"
 git push origin master
